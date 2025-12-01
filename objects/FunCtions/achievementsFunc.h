13093d99e7b7ec1e098e62413673912dff86cde3
Zemut <dmiguelsdasilv@proton.me>
2025-12-01 19:19:01 -0300

    More updates

--- objects/FunCtions/achievementsFunc.h
+++ objects/FunCtions/achievementsFunc.h
@@ -4,31 +4,32 @@ int gold;
 int silver;
 } Achievements;
 
+Achievements achievements;
 
 void Achievementsthing(){
-Achievements.bronze = 0;
-Achievements.gold = 0;
-Achievements.silver = 0;
+achievements.bronze = 0;
+achievements.gold = 0;
+achievements.silver = 0;
 }
 // Achievement system - START
 // Night achievements:
 void NightAchievements(int event){
 if (event == 1 && night == 1 && nightwon == 1){
-for (Achievements.bronze = 0; Achievements.bronze < 1; Achievements.bronze++){}
+for (achievements.bronze = 0; achievements.bronze < 1; achievements.bronze++){}
 }//Night 1
 if (event == 2 && night == 2 && nightwon == 1){
-for (Achievements.bronze = 1; Achievements.bronze < 2; Achievements.bronze++){}
+for (achievements.bronze = 1; achievements.bronze < 2; achievements.bronze++){}
 }//Night 2
 if (event == 3 && night == 3 && nightwon == 1){
-for (Achievements.silver = 0; Achievemnts.silver < 1; Achievements.silver++){}
+for (achievements.silver = 0; achievements.silver < 1; achievements.silver++){}
 }//Night 3
 if (event == 4 && night == 4 && nightwon == 1){
-for (Achievements.silver = 1; Achievements.silver < 2; Achievements.silver++){}
+for (achievements.silver = 1; achievements.silver < 2; achievements.silver++){}
 }//Night 4
 if (event == 5 && night == 5 && nightwon == 1){
-for (Achievements.silver = 2; Achievements.silver < 3; Achievements.silver++){}
+for (achievements.silver = 2; achievements.silver < 3; achievements.silver++){}
 }//Night 5
 if (event == 6 && night == 6 && nightwon == 1){
-for (Achievements.silver = 3; Achievements.silver < 4; Achievements.silver++){}  
+for (achievements.silver = 3; achievements.silver < 4; achievements.silver++){}  
 }//Night 6
 }
